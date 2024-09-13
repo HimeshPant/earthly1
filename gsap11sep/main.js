6d19d01f8ff4031c908e8d2197f80c1aac2e7d8b
HimeshPant <himeshpant0@gmail.com>
2024-09-13 07:41:07 +0530

    added gsap for counter

--- gsap11sep/main.js
+++ gsap11sep/main.js
@@ -4,3 +4,35 @@ document.addEventListener("mousemove",function(elem){
     top:elem.y
 })
 })
+var hi=gsap.timeline()
+hi.from(".line h1",{
+    y:150,
+    stagger:0.3,
+    duration:1,
+})
+hi.from(".loader",{
+    rotate:360,
+    repeat:-1,
+
+})
+gsap.to(".page1",{
+    onstart:function(){
+        var counter=document.querySelector(".counter h1");
+        var grow=0;
+        setInterval(()=>{
+            if(grow<100){
+                grow++;
+                counter.innerHTML=grow++;
+            }
+            else{
+                grow=100;
+                counter.innerHTML=grow;
+            }
+        },33);
+    }
+})
+hi.to(".page1",{
+    opacity:0,
+    // delay to be very carefully used as it will make repeting user experience bad
+    // delay:1
+})
