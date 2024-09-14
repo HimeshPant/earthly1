6d47d41783aa51ab6e1b8f5e0e1482600e45d302
HimeshPant <himeshpant0@gmail.com>
2024-09-14 17:20:00 +0530

    modified js file

--- gsap11sep/main.js
+++ gsap11sep/main.js
@@ -1,39 +1,44 @@
-document.addEventListener("mousemove",function(elem){
-    gsap.to("#cursor",{
-    left:elem.x,
-    top:elem.y
-})
-})
-var hi=gsap.timeline()
-hi.from(".line h1",{
-    y:150,
-    stagger:0.3,
-    duration:1,
-})
-hi.from(".loader",{
-    rotate:360,
-    repeat:-1,
+document.addEventListener("mousemove", function(elem) {
+    gsap.to("#cursor", {
+        left: elem.x,
+        top: elem.y
+    });
+});
 
+var hi = gsap.timeline();
+
+// Animate text appearance
+hi.from(".line h1", {
+    y: 150,
+    stagger: 0.3,
+    duration: 1
 })
-gsap.to(".page1",{
-    onstart:function(){
-        var counter=document.querySelector(".counter h1");
-        var grow=0;
-        setInterval(()=>{
-            if(grow<100){
+.from(".loader", {
+    rotate: 360,
+    repeat: -1,
+});
+
+// Counter animation
+gsap.to(".page1", {
+    onStart: function() {
+        var counter = document.querySelector(".counter h1");
+        var grow = 0;
+        setInterval(() => {
+            if (grow < 100) {
                 grow++;
-                counter.innerHTML=grow++;
+                counter.innerHTML = grow;
+            } else {
+                grow = 100;
+                counter.innerHTML = grow;
             }
-            else{
-                grow=100;
-                counter.innerHTML=grow;
-            }
-        },33);
+        }, 16);
     }
-})
-hi.to(".page1",{
-    
-    opacity:0,
-    // delay to be very carefully used as it will make repeting user experience bad
-    // delay:1,
-})
+});
+
+// Animation for .page1
+hi.to(".page1", {
+    opacity: 0,
+    backgroundColor: "#000", // Background color change to black
+    //  duration: 1, // Duration should match the opacity transition
+    // ease: "power1.inOut" // Optional: Adds easing for smoother transition
+});
